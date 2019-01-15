a64223d48a17480aa47cd5519384fa2e9e2f4ac0
Ben Houston <BenjaminAHouston@gmail.com>
2019-01-15 16:56:28 -0500

    Updates

---
+++ precache-manifest.d0b0348b3ebff5f71722436f7921aeb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "af2f98c8c914b16243fe",
+    "url": "/clickygame/static/js/main.af2f98c8.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clickygame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "af2f98c8c914b16243fe",
+    "url": "/clickygame/static/css/main.6b545dda.chunk.css"
+  },
+  {
+    "revision": "cfa9c5113b637cbedde5a4c63fa71128",
+    "url": "/clickygame/index.html"
+  }
+];
